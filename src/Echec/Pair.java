a99190f51f981bb115921846500bb8a0e9936445
Marcil <marcil.sebastien.2@ens.uqam.ca>
2023-11-27 12:57:45 -0500

    Je vous aime !!!

---
+++ src/Echec/Pair.java
@@ -0,0 +1,11 @@
+package Echec;
+
+public class Pair<T, T1> {
+    public T1 deuxieme;
+    public T1 premiere;
+
+    public Pair(T1 deuxieme, T1 premiere) {
+        this.deuxieme = deuxieme;
+        this.premiere = premiere;
+    }
+}
